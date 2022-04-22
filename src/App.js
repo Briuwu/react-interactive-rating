1a5a0dee754812b880c921c675206bf493178c7e
Briuwu7474 <millontebrian@gmail.com>
2022-04-22 18:14:32 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/main.css'
+import Rating from './components/Rating'
+import Thankyou from './components/Thankyou'
+import { useState } from 'react'
+import {nanoid} from 'nanoid'
 
 function App() {
+  const [rate, setRate] = useState(rateValue())
+  const [thanks, setThanks] = useState(false)
+
+  function generateRate(num) {
+    return {
+      value: num,
+      selected: false,
+      id: nanoid(),
+      selectedValue: ''
+    }
+  }
+
+  function rateValue() {
+    const newRate = []
+    for(let i = 1; i < 6; i++) {
+      newRate.push(generateRate(i))
+    }
+    return newRate
+  }
+
+
+  const rateElements = rate.map(item => {
+    return (
+      <div 
+        onClick={() => checkRate(item.id)} 
+        key={item.id} 
+        className={item.selected ? "rate selected" : "rate"}
+        >{item.value}
+      </div>
+    )
+  })
+
+  function checkRate(id) {
+    setRate(prev => prev.map(item => {
+      return id === item.id ? 
+        {...item, selected: true} : 
+        {...item, selected: false}
+    }))
+  }
+
+
+  function submitBtn() {
+    setThanks(true)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {thanks ? <Thankyou value={rate} /> : <Rating rateElements={rateElements}
+      onclick={submitBtn}
+      />}
     </div>
   );
 }
